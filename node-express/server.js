7dfc43bd024f3342b5233d73dcd6e7afc3af9cd9
cgroov <30381292+groov1234@users.noreply.github.com>
2018-02-12 15:32:25 -0500

    modified server.js to use server instead of app

--- node-express/server.js
+++ node-express/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const server = express();
 const PORT = 3030;
 
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 
 const users = [
   {
@@ -14,37 +14,37 @@ const users = [
   }
 ];
 
-app.use('/', (req, res, next) => {
+server.use('/', (req, res, next) => {
   console.log('Request type:', req.methhod);
   next();
 });
 
-app.post('/users', (req, res) => {
+server.post('/users', (req, res) => {
   res.send(user[req.params.users])
 })
 
-app.get('/users', (req, res) => {
+server.get('/users', (req, res) => {
   res.send(users[req.params.user] + "");
 });
 
-app.get('/users/:id', (req, res) => {
+server.get('/users/:id', (req, res) => {
   user[req.body.id] = userID;
   userId++
   res.send(userId + "");
 });
 
-app.get('/search?name=<query>', (req, res) => {
+server.get('/search?name=<query>', (req, res) => {
   res.send(name[req.query.user]);
 })
 
-app.delete('/users/:id', (req, res) => {
+server.delete('/users/:id', (req, res) => {
   
 })
 
-app.listen(PORT, err => {
+server.listen(PORT, err => {
   if (err) {
     console.log(`There was an error starting the server: $(err)`);
   } else {
-    console.log(`App Listening on Port ${PORT}`);
+    console.log(`Server Listening on Port ${PORT}`);
   }
 });
